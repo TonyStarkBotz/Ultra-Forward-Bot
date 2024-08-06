72fc8478c976a10bc5e9f9d0141b0b960ce10ab8
Tony Stark <138078424+TonyStarkBotz@users.noreply.github.com>
2024-08-06 21:13:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,13 @@
-#!/bin/bash
-
 echo "Cloning Repo...."
-
-if [ -z "$BRANCH" ]; then
+if [ -z $BRANCH ]
+then
   echo "Cloning main branch...."
-  git clone https://github.com/TonyStarkBotz/Ultra-Forward-Bot /Ultra-Forward-Bot
+  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot JishuDeveloper/Ultra-Forward-Bot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/TonyStarkBotz/Ultra-Forward-Bot -b "$BRANCH" /Ultra-Forward-Bot
+  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot -b $BRANCH /Ultra-Forward-Bot
 fi
-
-cd /Ultra-Forward-Bot || exit
-
+cd JishuDeveloper/Ultra-Forward-Bot
 pip3 install -U -r requirements.txt
-
 echo "Starting Bot...."
 python3 main.py
